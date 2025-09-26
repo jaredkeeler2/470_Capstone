07428bc9560853b5af248f220f1acd2c9939c7e8
Jared Keeler <jkeeler2@alaska.edu>
2025-09-25 20:34:39 -0800

    Added scraper function for curric website and updated views

--- main/csce_scraper.py
+++ main/csce_scraper.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-from .models import Course
+from collections import defaultdict
+import re
 
+'''
 #Catalog CSCE page
 URL = "https://catalog.uaa.alaska.edu/undergraduateprograms/coeng/bs-computerscience/"
 
-#CSCE scraper function
+#CSCE catalog scraper function
 def csce_courses(url=URL):
     response = requests.get(url, timeout=20)    #downloads the html (will wait 20 seconds before timeout)
     response.raise_for_status()    #throws error if the download failed
@@ -13,7 +15,7 @@ def csce_courses(url=URL):
     codes = set()   #The set will only store unique values without duplicated
 
     #this loops through all of the <a> tags in <td class="codecol">
-    for a in soup.select("td.codecol a"):
+    for a in soup.select("td.codecol a"):   #got the class links in code column
         text = a.get_text(" ", strip=True).replace("\xa0", " ")    #retrieves the text and replaces weird \xa0 spaces with normal spaces
         if not text.startswith(("CSCE", "CSCE/")):    #skips anything not starting with CSCE or CSCE/
             continue
@@ -22,10 +24,50 @@ def csce_courses(url=URL):
         if "/" in parts[0] and len(parts) > 1:
             text = "CSCE " + " ".join(parts[1:])
         codes.add(text)    #adds and saves the new text to the course codes
-        
-        saved_courses = []  #creating an empty lists
-        for code in sorted(codes): #Loop through the sorted(code)
-            course, created = Course.objects.get_or_create(code=code)  # skip if object exists else create in DB
-            saved_courses.append(course)
-        
+
     return sorted(codes)    #sorted order of the list
+'''
+    
+def schedule_scraper(term="202503", subj="CSCE"):
+    url = "https://curric.uaa.alaska.edu/ajax/ajaxScheduleSearch.php"   #API url
+    params = {"term": term, "subj": subj}
+    response = requests.get(url, params=params, timeout=20)     #requesting the specific parameters from the API, timeout if it takes more than 20s
+    response.raise_for_status() #throws error if the download failed
+
+    rows = response.json()  #convert json array of objects to python list of dictionaries
+    totals = defaultdict(int)   #totals keep track of total students per course, each new key value is set to 0
+    for row in rows:    #iterates through the dictonaries to extract subject, course, title, and enrollment number
+        if row.get("subj").upper() == "CSCE":   #only for CSCE
+            crs = row["crs"].upper()
+            if not crs.endswith("L"):   #if it is a lab it skips (since the same students in the lab class are in the normal class)
+                num = int(re.search(r'\d+', crs).group())  #regular expression that finds the string of numbers, and converts to integers
+                if 100 <= num < 500:    #since we are only gathering data for 100-400 level courses
+                    code = row['subj'].upper() + " " + row['crs']   #concatenates the subject and the course (e.g. CSCE A101)
+                    title = row['title']
+                    enrolled = int(row.get("enrolled")) 
+                    totals[code, title] += enrolled  #sums up the enrollment for one course
+    results = []
+    for item in sorted(totals.items()):
+        #current item looks like (("CSCE A101", "Intro to CS"), 54)
+        key = item[0]       #first index of item is ("CSCE A101", "Intro to CS")
+        total = item[1]     #second index is the enrollment count (e.g. 54)
+        code = key[0]       #first part in the key is the course name (e.g. "CSCE A101")
+        title = key[1]      #second part in the key is course title (e.g. "Intro to CS")
+
+        course_data = (code, title, total)
+        results.append(course_data)     #add the data to the results list
+    return results
+
+#Example usage
+if __name__ == "__main__":
+    terms = ["202503", "202502", "202501"]  #fall, summer, spring
+    whole_year = {} #dictionary to store the scraper's results
+
+    for t in terms:     #iterate through each term
+        whole_year[t] = schedule_scraper(term=t)    #stores each term with the results for that term
+
+    # print results
+    for term, results in whole_year.items():    #splits the tuple value into two variables (the term and the results)
+        print("\n===== Term " + term + " =====")
+        for course_data in results:     #prints all of the results for each term
+            print(course_data)
--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from .csce_scraper import csce_courses
+from .csce_scraper import schedule_scraper
 
 #homepage
 def home(request):
-    courses = csce_courses()  #scrapes that page
-    return render(request, "home.html", {"courses": courses})
+    results = schedule_scraper()  #scrapes that page
+    course_codes = []
+    for course in results:      #loops through each tuple in the results and gets the first value and adds it to the list for the course name dropdown
+        code = course[0]
+        course_codes.append(code)
+    return render(request, "home.html", {"courses": course_codes})
